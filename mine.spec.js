1e99c27166d09a9177ec5b1122269bf8b5fa8ad9
Jon Fazzaro <JFazzaro@apterainc.com>
2021-02-09 16:41:03 -0500

    wip: testing

--- mine.spec.js
+++ mine.spec.js
@@ -3,13 +3,21 @@ jest.mock('node-fetch');
 const mine = require("./mine");
 const fetch = require("node-fetch");
 
-fetch.mockImplementation(url => {
-    console.log("fetch was called with URL " + url);
-    return Promise.resolve({ json: () => { return { reminders: [] } } });
-});
-
 describe("The miner", () => {
     it("is under test", (done) => {
+        mocked.reminders = [{ complete_ts: 0 }, {}];
         mine({ done });
+        expect(last.fetchedUrl).toContain("https://slack.com/api/reminders.list");
     });
+});
+
+const last = {};
+const mocked = {
+    reminders: []
+};
+
+fetch.mockImplementation((url, options) => {
+    last.fetchedUrl = url;
+    last.fetchedOptions = options;
+    return Promise.resolve({ json: () => { return { reminders: mocked.reminders } } });
 });
