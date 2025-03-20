6f7c57cc1c11c803184d30517e6b1635aa7b9538
Jon Fazzaro <jon@fazzaro.com>
2025-03-20 13:20:48 -0400

    clean: compile works without being a Node module

--- CardMeShortcut/compile.js
+++ CardMeShortcut/compile.js
@@ -1,13 +1,19 @@
-import {minify} from "minify";
-import encodeUrl from "encodeurl";
-import dotenv from "dotenv";
+const compressor = require('node-minify');
+const encodeUrl = require("encodeurl");
+const dotenv = require("dotenv");
 
 dotenv.config();
+main();
 
-console.log("javascript:" 
-    + encodeUrl(
-        withKeys(
-            await minify("./CardMeShortcut/shortcut.js"))));
+async function main() {
+    await compressor.minify({
+        compressor: 'terser',
+        input: "./CardMeShortcut/shortcut.js",
+        output: "./CardMeShortcut/shortcut.min.js",
+    }).then(minified => {
+        console.log("javascript:" + encodeUrl(withKeys(minified)));
+    }).catch(err => console.error(err));
+}
 
 function withKeys(minified) {
     return minified.replace(/process\.env\.(\w+)/g,
--- CardMeShortcut/shortcut.js
+++ CardMeShortcut/shortcut.js
@@ -1,32 +1,29 @@
-(function () {
-    Promise.all(Array.from(document.querySelectorAll(".p-saved_for_later_page__list_wrapper .c-virtual_list__item"))
-        .map(element => {
-            const message = {
-                sender: element.querySelector(".p-activity_ia4_page__item__senders").innerText,
-                text: "> " + element.querySelector(".p-activity_ia4_page__item__message").innerText,
-                url: "https://app.slack.com/client/" + process.env.slackClient + "/archives/" + element.getAttribute("data-item-key").split("_")[0].replace("-", "/p").replace(".", "")
-            }
+Promise.all(Array.from(document.querySelectorAll(".p-saved_for_later_page__list_wrapper .c-virtual_list__item"))
+    .map(element => {
+        const message = {
+            sender: element.querySelector(".p-activity_ia4_page__item__senders").innerText,
+            text: "> " + element.querySelector(".p-activity_ia4_page__item__message").innerText,
+            url: "https://app.slack.com/client/" + process.env.slackClient + "/archives/" + element.getAttribute("data-item-key").split("_")[0].replace("-", "/p").replace(".", "")
+        }
 
-            return fetch("https://api.trello.com/1/cards", {
-                headers: {"Content-Type": "application/json"},
-                method: "POST",
-                body: JSON.stringify({
-                    token: process.env.trelloToken,
-                    key: process.env.trelloKey,
-                    idList: process.env.targetListID,
-                    name: "Respond: " + message.sender,
-                    desc: message.text,
-                    // due: due,
-                    urlSource: message.url,
-                })
+        return fetch("https://api.trello.com/1/cards", {
+            headers: {"Content-Type": "application/json"},
+            method: "POST",
+            body: JSON.stringify({
+                token: process.env.trelloToken,
+                key: process.env.trelloKey,
+                idList: process.env.targetListID,
+                name: "Respond: " + message.sender,
+                desc: message.text,
+                urlSource: message.url,
             })
-                .then(res => {
-                    element.querySelector("[aria-label='Mark complete']").click();
-                    return message;
-                })
-                .catch(err => console.log(err));
-        })).then(cards => {
+        })
+            .then(res => {
+                element.querySelector("[aria-label='Mark complete']").click();
+                return message;
+            })
+            .catch(err => console.log(err));
+    })).then(cards => {
         const count = cards.length || "No"
         alert(count + " response cards added!")
-    })
-}())
+    });
