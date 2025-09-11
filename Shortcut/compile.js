f86a6641618ffdc553ab28ad487c6cffb6fa792a
jonfazzaro <jon@fazzaro.com>
2025-09-11 08:25:40 -0400

    feat: compile for Mac Shortcuts

--- Shortcut/compile.js
+++ Shortcut/compile.js
@@ -1,21 +1,32 @@
 const compressor = require('node-minify');
 const encodeUrl = require("encodeurl");
 const dotenv = require("dotenv");
+const fs = require('fs')
 
 dotenv.config();
 main();
 
+function readScript() {
+  return withKeys(fs.readFileSync('./Shortcut/shortcut.js').toString());
+}
+
 async function main() {
-    await compressor.minify({
-        compressor: 'uglify-es',
-        input: "./Shortcut/shortcut.js",
-        output: "/dev/null",
-    }).then(minified => {
-        console.log("javascript:" + encodeUrl(withKeys(minified)));
-    }).catch(err => console.error(err));
+  // const script = await minified();
+  const script = readScript();
+  console.log(script);
+}
+
+async function minified(script) {
+  await compressor.minify({
+    compressor: 'uglify-es',
+    content: script,
+    // output: "/dev/null",
+  }).then(minified => {
+    console.log("javascript:" + encodeUrl(withKeys(minified)));
+  }).catch(err => console.error(err));
 }
 
 function withKeys(minified) {
-    return minified.replace(/process\.env\.(\w+)/g,
-        (_, key) => `"${process.env[key]}"`);
+  return minified.replace(/process\.env\.(\w+)/g,
+    (_, key) => `"${process.env[key]}"`);
 }
